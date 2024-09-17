163ce635c3d413bcdbd4a6831267de240f51a27b
Alishandik <Babayev.alishka@gmail.com>
2024-09-17 17:08:03 +0300

    Add files via upload

---
+++ Module_1_6.py..py
@@ -0,0 +1,18 @@
+my_dict={"Ali": 2001, "Anya": 2000}
+print(my_dict)
+print(my_dict.get("Ali"))
+print(my_dict.get("Sasha"))
+my_dict=["Moscow"]
+my_dict=["Manager"]
+print(my_dict)
+ # Не могу понять, как правильно решить этот пункт, объясните пожалуйста. Спасибо (- Удалите одну из пар в словаре по существующему ключу из словаря my_dict и выведите значение из этой пары на экран.
+#   - Выведите на экран словарь my_dict.)
+
+
+#множества
+my_set = {1, 'apple', 3.14, 'apple', True}
+print(my_set)
+my_set.add('banana')
+my_set.add(False)
+my_set.remove(1)
+print(my_set)
